597e2319f7bbc697649b3e775b32cbadef319f1f
hmcdowell95 <hmcdowell95@github.com>
2020-10-27 19:19:21 -0400

    fill notecard controller

--- notecard-backend/app/controllers/notecards_controller.rb
+++ notecard-backend/app/controllers/notecards_controller.rb
@@ -1,2 +1,23 @@
 class NotecardsController < ApplicationController
+    def index
+        notecards = Notecard.all 
+        render json: notecards, only: [:id, :term, :definition]
+    end
+
+    def create
+        notecard = Notecard.create(term: params[:term], definition: params[:definition], subject_id: params[:subject_id])
+        render json: notecard, only: [:id, :term, :definition, :subject_id]
+    end
+
+    def update
+        notecard = Notecard.find(params[:id])
+        notecard.update(term: params[:term], definition: params[:definition])
+        render json: notecard, only: [:id, :term, :definition, :subject_id]
+    end
+
+    def destroy
+        notecard = Notecard.find(params[:id])
+        notecard.destroy
+    end
+
 end

e18d957a0dd7a664515244f887d545e0fdebf62b
hmcdowell95 <hmcdowell95@github.com>
2020-10-27 19:08:57 -0400

    fill subject controller

--- notecard-backend/app/controllers/subjects_controller.rb
+++ notecard-backend/app/controllers/subjects_controller.rb
@@ -1,3 +1,23 @@
 class SubjectsController < ApplicationController
+    def index
+        subjects = Subject.all 
+        render json: subjects, only: [:id, :name]
+    end
+
+    def create
+        subject = Subject.create(name: params[:name])
+        render json: subject, only: [:id, :name]
+    end
+
+    def update
+        subject = Subject.find(params[:id])
+        subject.update(name: params[:name])
+        render json: subject, only: [:id, name]
+    end
+
+    def destroy
+        subject = Subject.find(params[:id])
+        subject.destroy
+    end
     
 end

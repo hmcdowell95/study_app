de8a52ed359e0b4ce127c64d661177c3826a0193
hmcdowell95 <hmcdowell95@github.com>
2020-10-27 22:14:39 -0400

    init note reducer

---
+++ src/reducers/notecard-reducer.js
@@ -0,0 +1,12 @@
+function notecardReducer(state = {notes: [], loading: false}, action) {
+    switch (action.type) {
+        case "ADD_NOTE":
+            return {
+                ...state,
+                notes: [...state.notes, action.note],
+                loading: false
+            }
+        default: 
+            return state;
+    }
+}

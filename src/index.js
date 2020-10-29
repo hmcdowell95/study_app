f522f0bd4144be0d32512666dd29bef63ad5d6e1
hmcdowell95 <hmcdowell95@github.com>
2020-10-29 17:59:07 -0400

    create store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import notecardReducer from './reducers/notecard-reducer';
+import subjectReducer from './reducers/subject-reducer';
+
+const rootReducer = combineReducers({
+  subjects: subjectReducer,
+  notecards: notecardReducer
+});
+
+let store = createStore(rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );

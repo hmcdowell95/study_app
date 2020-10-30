6fde2c8304c67f76c2ef0685473b6397004dd09f
hmcdowell95 <hmcdowell95@github.com>
2020-10-29 21:24:39 -0400

    add Route to subject container

--- src/containers/subject-container.js
+++ src/containers/subject-container.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
 class SubjectContainer extends Component {
 
     render() {
         return(
-            <div>list of subjects with links</div>
+            <div>list of subjects with links
+                <Route exact path={this.props.match.url} render={() => <div>test</div>} />
+            </div>
         )
     }
 }

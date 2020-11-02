94d855563349fa2de27e4992127e446fd6f68b36
hmcdowell95 <hmcdowell95@github.com>
2020-11-01 19:36:03 -0500

    fix routing

--- src/components/subject.js
+++ src/components/subject.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import NoteCardContainer from '../containers/notecard-container';
 
 class Subject extends Component {
+
+    renderSubject = () => {return this.props.subjects.find(x => x.id === parseInt(this.props.match.params.id))}
+
     render() {
         return(
             <div>
-                <h2>{this.props.subjects.find(s => s.id === this.props.match.params.id).name}</h2>
+                <h2>{this.renderSubject().name}</h2>
                 <NoteCardContainer subjectId={this.props.match.params.id} />
             </div>
         )

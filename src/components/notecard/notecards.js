a91a64f5c6a7681805f19da438a3e3a0aa7aa2b8
hmcdowell95 <hmcdowell95@github.com>
2020-11-06 16:18:45 -0500

    edit README

--- src/components/notecard/notecards.js
+++ src/components/notecard/notecards.js
@@ -3,7 +3,9 @@ import NoteCard from './notecard';
 import NavBar from '../../containers/NavBar';
 
 const NoteCards = (props) => 
-<div><NavBar/>
-<div class="grid">{props.notes.map(n => <NoteCard key={n.id} note={n} />)}</div></div>
+<div>
+    <NavBar/>
+    <div class="grid">{props.notes.map(n => <NoteCard key={n.id} note={n} />)}</div>
+</div>
 
 export default NoteCards
